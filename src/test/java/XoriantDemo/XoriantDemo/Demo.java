742086d21c854685a05c4abffc559a5efaf9cfd3
sharad <sharad.mali@elliemae.com>
2019-02-11 11:56:50 +0530

    test

---
+++ src/test/java/XoriantDemo/XoriantDemo/Demo.java
@@ -0,0 +1,43 @@
+package XoriantDemo.XoriantDemo;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Demo {
+	
+	@Test
+	public void add() {			
+		
+		System.out.println("---------Addition");
+		int a = 10;
+		int b = 20;
+		Assert.assertEquals(30, a+b);
+	}
+	
+	@Test
+	public void sub() {	
+		
+		System.out.println("---------Subtraction");
+		int a = 10;
+		int b = 20;
+		Assert.assertEquals(10, b-a);
+	}
+	
+	@Test
+	public void mul() {	
+		
+		System.out.println("---------Multiplication");
+		int a = 10;
+		int b = 20;
+		Assert.assertEquals(200, a*b);
+	}
+	
+	@Test
+	public void div() {
+		
+		System.out.println("---------Division");
+		int a = 10;
+		int b = 20;
+		Assert.assertEquals(2, b/a);
+	}
+}
