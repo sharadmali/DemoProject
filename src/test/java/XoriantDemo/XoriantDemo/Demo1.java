791ec886c83d8597c3520e1172bde4d10c29cdfd
sharadmali <sharad.mali24@gmail.com>
2019-03-21 14:18:56 +0530

    added mul_01 in demo1

--- src/test/java/XoriantDemo/XoriantDemo/Demo1.java
+++ src/test/java/XoriantDemo/XoriantDemo/Demo1.java
@@ -30,4 +30,13 @@ public class Demo1 {
 		int b = 20;
 		Assert.assertEquals(200, a*b);
 	}
+	
+	@Test
+	public void mul_01() {	
+		
+		System.out.println("---------Multiplication");
+		int a = 10;
+		int b = 20;
+		Assert.assertEquals(200, a*b);
+	}
 }
