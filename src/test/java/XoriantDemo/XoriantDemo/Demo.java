654e94ce6834f46ce3b7af63c667c652cbf081ea
sharad <sharad.mali@elliemae.com>
2019-04-03 19:42:52 +0530

    added div_01 by me

--- src/test/java/XoriantDemo/XoriantDemo/Demo.java
+++ src/test/java/XoriantDemo/XoriantDemo/Demo.java
@@ -58,4 +58,13 @@ public class Demo {
 		int b = 20;
 		Assert.assertEquals(2, b/a);
 	}
+	
+	@Test
+	public void div_03() {
+		
+		System.out.println("---------Division");
+		int a = 10;
+		int b = 20;
+		Assert.assertEquals(2, b/a);
+	}
 }
