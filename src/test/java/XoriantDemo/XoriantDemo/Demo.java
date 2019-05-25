08608afc1da20e201ad497e012cf09aed42cfaa1
sharad <sharad.mali@elliemae.com>
2019-05-25 21:19:47 +0530

    updated Demo

--- src/test/java/XoriantDemo/XoriantDemo/Demo.java
+++ src/test/java/XoriantDemo/XoriantDemo/Demo.java
@@ -1,11 +1,22 @@
 package XoriantDemo.XoriantDemo;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -15,53 +26,77 @@ public class Demo {
 	public WebDriver driver;
 	
 	@Test
-	public void launch() {
+	public void launch() throws InterruptedException {
 		
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
+		WebDriver driver = null;
 		
-		driver.manage().window().maximize();
-		driver.manage().deleteAllCookies();
-		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		driver.navigate().to("http://www.airindia.in");
-		WebElement elm = driver.findElement(By.xpath("//a[text()='Home']"));
+		String browser = "chrome";
+		//String browser = "ie";
+		
+		switch (browser.toLowerCase()) {
+		
+		case "chrome":
+			WebDriverManager.chromedriver().setup();
+			DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+			capabilities.setCapability("chrome.switches", Arrays.asList("--always-authorize-plugins"));
+
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--disable-popup-blocking");
+			options.addArguments("--disable-extensions");
+			options.addArguments("--incognito");
+			options.addArguments("--no-sandbox");
+			options.addArguments("start-maximized");
+			options.addArguments("--headless");
+			options.addArguments("--disable-dev-shm-usage");
+			options.setExperimentalOption("useAutomationExtension", false);
+			capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+
+			driver = new ChromeDriver(capabilities);	
+			break;
+			
+		case "ie":
+			WebDriverManager.iedriver().setup();						
+			DesiredCapabilities caps = DesiredCapabilities.internetExplorer();
+			
+			//caps.setCapability(InternetExplorerDriver.IGNORE_ZOOM_SETTING, true);
+			caps.setCapability(InternetExplorerDriver.REQUIRE_WINDOW_FOCUS, true);
+			caps.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+			caps.setCapability(InternetExplorerDriver.IGNORE_ZOOM_SETTING, true);
+			caps.setCapability(InternetExplorerDriver.FORCE_CREATE_PROCESS, true);
+			caps.setCapability(InternetExplorerDriver.IE_SWITCHES, "-private");
+			caps.setCapability(InternetExplorerDriver.NATIVE_EVENTS, false);			
+			caps.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+			
+			driver = new InternetExplorerDriver(caps);
+			break;		
+		}
+		
+		driver.get("http://www.google.com");
+		WebElement element = driver.findElement(By.name("q"));
+		element.sendKeys("Guru99");
+		element.submit();
+		
+		Thread.sleep(5000);
+		JavascriptExecutor je = (JavascriptExecutor) driver;		
+		WebElement elm = driver.findElement(By.xpath("//*[@id='gb_70']"));
+		je.executeScript("arguments[0].scrollIntoView(true);",elm);
+		
 		elm.click();
-		System.out.println("---------CLick on Home link");
-	}
-	@Test
-	public void add() {			
 		
-		System.out.println("---------Addition");
-		int a = 20;
-		int b = 20;		
-		Assert.assertEquals(40, a+b);
-	}
-	
-	@Test
-	public void sub() {	
+		File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		try {			
+			FileUtils.copyFile(src, new File("C:\\Temp\\error.png"));
+		}
+
+		catch (IOException e) {
+			System.out.println(e.getMessage());
+
+		}
 		
-		System.out.println("---------Subtraction");
-		int a = 10;
-		int b = 20;
-		Assert.assertEquals(10, b-a);
-	}
+		System.out.println("Page title is: " + driver.getTitle());
+		driver.quit();
 		
-	@Test
-	public void mul() {	
-		
-		System.out.println("---------Multiplication");
-		int a = 10;
-		int b = 20;
-		Assert.assertEquals(200, a*b);
 	}
 	
-	@Test
-	public void div() {
 		
-		System.out.println("---------Division");
-		int a = 10;
-		int b = 20;
-		Assert.assertEquals(2, b/a);
-	}		
 }
